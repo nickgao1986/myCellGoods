e5f1e92069fdff049b1452a445944e04539827c0
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-09 07:44:00 +0800

    fix bug

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,17 +3,19 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight' :totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
         </div>
-        <div class="price">12</div>
+
+        <div class="price" :class="{'highlight':totalCount>0}">{{totalPrice}}</div>
         <div class="desc">另需配送费{{deliveryPrice}}元</div>
 
       </div>
       <div class="content-right">
-        <div class="pay">
-          {{minPrice}}起送
+        <div class="pay" :class="payClass">
+          {{payDesc}}
         </div>
       </div>
     </div>
@@ -28,8 +30,8 @@
         default() {
           return [
             {
-              price: 10,
-              count: 1
+                price: 10,
+                count: 2
             }
           ];
         }
@@ -41,6 +43,39 @@
       minPrice: {
         type: Number,
         default: 0
+      }
+    },
+    computed: {
+        totalPrice() {
+            let total = 0;
+            this.selectFoods.forEach((food) => {
+              total += food.price * food.count;
+            });
+            return total;
+        },
+        totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+            return this.minPrice + '元起送';
+        } else if (this.totalPrice < this.minPrice) {
+            let def = this.minPrice - this.totalPrice;
+            return '还差' + def + '元';
+        } else {
+            return '去结算';
+        }
+      },
+      payClass() {
+         if (this.totalPrice < this.minPrice) {
+             return 'not-enough';
+         } else {
+             return 'enough';
+         }
       }
     }
   };
@@ -80,10 +115,28 @@
             border-radius 50%
             background: #2b343c
             text-align center
+            &.highlight
+              background: rgb(0,160,220)
             .icon-shopping_cart
               font-size: 24px
               line-height: 48px
               color: #80858a
+              &.highlight
+                color: #fff
+          .num
+              position: absolute
+              top: 0
+              right 0
+              width: 24px
+              height 16px
+              line-height 16px
+              text-align center
+              border-radius 16px
+              font-size 9px
+              font-weight 700
+              color #fff
+              background-color rgb(240,20,20)
+              box-shadow 0 4px 8px 0 rgb(0,0,0,0.4)
         .price
           display: inline-block
           vertical-align: top
@@ -94,6 +147,8 @@
           border-right: 1px solid rgba(255, 255, 255, 0.1)
           font-size: 16px
           font-weight: 700
+          &.highlight
+            color: #fff
         .desc
           display:inline-block
           vertical-align top
@@ -111,4 +166,9 @@
           text-align center
           font-weight: 700
           background: #2b333b
+          &.not_enough
+            background: #2b333b
+          &.enough
+            background: #00b43c
+            color: #fff
 </style>
