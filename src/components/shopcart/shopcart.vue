07819d31ef47e571644e7b9e07a431e77d67d6f7
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-11 09:07:25 +0800

    fix bug

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -13,7 +13,7 @@
         <div class="desc">另需配送费{{deliveryPrice}}元</div>
 
       </div>
-      <div class="content-right">
+      <div class="content-right" @click.stop.prevent="pay">
         <div class="pay" :class="payClass">
           {{payDesc}}
         </div>
@@ -26,17 +26,17 @@
       </div>
     </div>
 
-    <div class="shopcast-list" v-show="listShow">
+    <div class="shopcast-list" v-show="listShow" transition="fold">
       <div class="list-header">
           <h1 class="title">购物车</h1>
-          <span class="empty">清空</span>
+          <span class="empty" @click="empty">清空</span>
       </div>
-      <div class="list-content">
+      <div class="list-content" v-el:list-content>
         <ul>
           <li class="food1" v-for="food in selectFoods">
               <span class="name">{{food.name}}</span>
               <div class="price">
-                <span>{{food.price*food.count}}</span>
+                <span>￥{{food.price*food.count}}</span>
               </div>
               <div class="cartcontrol-wrapper">
                 <cartcontrol :food="food"></cartcontrol>
@@ -46,10 +46,12 @@
       </div>
     </div>
   </div>
+  <div class="list-mask" v-show="listShow" @click="hideList" transition="fade"></div>
 </template>
 
 <script type="text/ecmascript-6">
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import BScroll from 'better-scroll';
   export default{
     props: {
       selectFoods: {
@@ -134,6 +136,17 @@
               return false;
             }
             let show = !this.fold;
+            if (show) {
+              this.$nextTick(() => {
+                  if (!this.scroll) {
+                    this.scroll = new BScroll(this.$els.listContent, {
+                      click: true
+                    });
+                  } else {
+                    this.scroll.refresh();
+                  }
+              });
+            }
             return show;
       }
     },
@@ -149,11 +162,26 @@
           }
         }
       },
+      empty() {
+          this.selectFoods.forEach((food) => {
+            food.count = 0;
+          });
+      },
       toggleList() {
           if (!this.totalCount) {
               return;
           }
           this.fold = !this.fold;
+      },
+      hideList() {
+          this.fold = true;
+      },
+      pay() {
+          if (this.totalPrice < this.minPrice) {
+              return;
+          }
+          let price = this.totalPrice;
+          window.alert('支付' + price);
       }
     },
     transitions: {
@@ -199,6 +227,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
   .shopcart
     position:fixed
     left: 0
@@ -304,62 +333,77 @@
             transition all 0.4s linear
     .shopcast-list
       width: 100%
-      height 258px
-      position fixed
-      bottom 48px
-      z-index: 100
+      position absolute
+      z-index: -1
       left 0
+      &.fold-transition
+        transition all 0.5s
+        transform translate3d(0,-100%,0)
+      &.fold-enter,&.fold-leave
+        transform translate3d(0,0,0)
       .list-header
-        display: flex
-        position:relative
-        left: 0
-        top: 0
-        z-index: 50
         background #f3f5f7
         line-height 40px
-        width 100%
+        height 40px
+        padding 0 18px
+        border-bottom 1px solid rgba(7,17,27,0.1)
         .title
-          align-items center
+          float left
           font-size 14px
           font-weight 200
           color: rgb(7,17,27)
           text-align center
           line-height 40px
-          margin-left 18px
         .empty
-          flex 1
-          text-align right
+          float right
           color: rgb(0,160,220)
           font-size 12px
           line-height 40px
           align-items center
-          margin-right 18px
       .list-content
+        padding 0 18px
+        max-height 217px
+        overflow hidden
         position relative
         top 0
         left 0
-        background-color #fff
+        background #fff
         height 218px
         width 100%
         .food1
-          width 100%
-          display flex
-          height 48px
-          align-items center
+          position relative
+          box-sizing border-box
+          padding 12px 0
+          border-1px(rgba(7,17,27,0.1))
           .name
-            display block
-            margin-left 18px
+            line-height 24px
             font-size 14px
-            text-align center
-            align-items center
+            color rgb(7,17,27)
           .price
-            display block
+            position absolute
+            right 110px
+            bottom 12px
+            line-height 24px
             font-size 14px
-            align-items center
-            margin-right: 12px
-            flex 1
-            text-align right
+            color rgb(240,20,20)
+            font-weight 700
           .cartcontrol-wrapper
-            display inline-block
-            margin-right 18px
+            position absolute
+            right 20px
+            bottom 6px
+  .list-mask
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index 40
+    backdrop-filter: blur(10px)
+    &.fade-transition
+      transition all 0.5s
+      background rgba(7,17,27,0.6)
+      opacity 1
+    &.fade-enter, &.fade-leave
+      opacity 0
+      background rgba(7,17,27,0)
   </style>
