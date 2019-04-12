fe039af98cb5d819676c5958251c7078c29721c3
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-12 22:01:40 +0800

    fix bug

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -41,6 +41,23 @@
       <div class="rating">
         <h1 class="title">商品评价</h1>
         <ratingselect :select-type="selectType" :only-content="OnlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img class="avatar" width="12" height="12" :src="rating.avatar">
+                </div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
+                 <p class="text">
+                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                 </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+
+        </div>
       </div>
     </div>
   </div>
@@ -50,6 +67,7 @@
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
+  import {formatDate} from 'common/js/date';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
   import split from 'components/split/split';
   import ratingselect from 'components/ratingselect/ratingselect';
@@ -78,13 +96,33 @@
             }
           };
       },
+      filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+     },
+      events: {
+        'ratingtype.select'(type) {
+            this.selectType = type;
+            this.$nextTick(() => {
+              this.scroll.refresh();
+            });
+        },
+        'content.toggle'(onlyContent) {
+            this.onlyContent = onlyContent;
+            this.$nextTick(() => {
+              this.scroll.refresh();
+          });
+        }
+      },
       methods: {
           show() {
               console.log('aaaafoodshow');
               this.showFlag = true;
               this.All = true;
               this.selectType = 0;
-              this.onlyContent = true;
+              this.onlyContent = false;
 
               this.$nextTick(() => {
               if (!this.scroll) {
@@ -104,12 +142,24 @@
             this.$dispatch('cart.add', event.target);
 
             Vue.set(this.food, 'count', 1);
+          },
+          needShow(type, text) {
+            console.log('onlyContent=' + this.onlyContent + 'text=' + text);
+            if (this.onlyContent && !text) {
+              return false;
+            }
+            if (this.selectType === ALL) {
+                return true;
+            } else {
+                return type === this.selectType;
+            }
           }
       }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .food
     position: fixed
     left: 0
@@ -212,8 +262,6 @@
         line-height 24px
         color rgb(77,85,93)
         padding 0 8px
-
-
     .rating
       padding-top 18px
       .title
@@ -221,5 +269,45 @@
         margin-left 18px
         font-size 14px
         color rgb(7,17,27)
-
+      .rating-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7,17,27,0.1))
+          .user
+            position absolute
+            right 0
+            top 16px
+            line-height 12px
+            font-size 0
+            .name
+              display:inline-block
+              margin-right 6px
+              vertical-align top
+              font-size 10px
+              color rgb(147,153,159)
+            .avatar
+              border-radius 50%
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(147,153,159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7,17,27)
+            .icon-thumb_up, .icon-thumb_down
+              margin-right 4px
+              line-height 24px
+              font-size 12px
+            .icon-thumb_up
+              rgb(0,160,220)
+            .icon-thumb_down
+              rgb(147,153,159)
+        .no-rating
+          padding 16px 0
+          font-size 12px
+          color rgb(147,153,159)
 </style>
